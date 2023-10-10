2da613700ad547806c7db8016cf113a47347e6d5
RichyCarballo <146492125+RichyCarballo@users.noreply.github.com>
2023-10-10 00:14:33 -0600

    mis primeros programas
    programa 6, 7, 8, 9, 10, 11

---
+++ Unidad2/Programa9.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace p9 // Note: actual namespace depends on the project name.
+{
+    internal class Calculadora
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c,d, e, f;
+            Console.WriteLine("Introduce primer valor");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introduce segundo valor");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            c = a + b;
+            Console.WriteLine("La suma es igual a :" + c);
+            d = a - b;
+            Console.WriteLine("La resta es igual a :" + d);
+            e = a * b;
+            Console.WriteLine("La multiplicación es igual a :" + e);
+            f = a / b;
+            Console.WriteLine("La división es igual a :" + f);
+        }
+    }
+}

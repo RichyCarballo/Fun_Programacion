4dd4618dba0c6d3fb2855cf86c250cc829715d1d
RichyCarballo <146492125+RichyCarballo@users.noreply.github.com>
2023-11-13 21:09:31 -0400

    Add files via upload

---
+++ Unidad 4/p2_u4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace U4programa2 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] calf = new double[10] { 100, 100, 100, 100, 100, 100, 100, 100, 100, 100 };
+            double promedio = 0, suma = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                suma = suma + calf[i];
+                promedio = suma / 10;
+            }
+            Console.WriteLine("El promedio de tus califiaciones es: " + promedio);
+        }
+    }
+}

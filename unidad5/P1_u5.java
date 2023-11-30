ec4051f6ee8741250c0cb2237d3372b96445dbb1
RichyCarballo <146492125+RichyCarballo@users.noreply.github.com>
2023-11-29 21:19:30 -0400

    Add files via upload

---
+++ unidad5/P1_u5.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.p1_u5;
+
+/**
+ *
+ * @author LaboratorioLTI2
+ */
+public class P1_u5 {
+
+    public static void main(String[] args) {
+        System.out.println(suma(7,8));
+    }
+    
+    
+    //metodo o funcion
+    public static float suma(float a, float b){
+        float r = a + b;
+        return r;
+    }
+}
